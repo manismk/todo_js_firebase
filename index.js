67af015e10f32f26f1b77f189d991b96b0b0c813
manismk <manismkmech@gmail.com>
2021-04-18 16:02:09 +0530

    finshed crud opertion

--- index.js
+++ index.js
@@ -20,38 +20,108 @@ const signOut = document.querySelector(".signOut");
 const todolist = document.querySelector(".todoList");
 const addTodo = document.querySelector(".addTodo");
 const todo = document.querySelector("#todo");
-
+let userUid;
 if (window.location.pathname === "/todo.html") {
   updateUserData();
 }
+
+function renderUserData(doc) {
+  let color;
+  if (doc.data().complete === false) {
+    color = "green";
+  } else {
+    color = "red";
+  }
+
+  let divList = `
+  <div class="list ${color}" data-id='${doc.id}'>
+    <p class="todoText">${doc.data().text}</p>
+    <div class="doneDelete">
+      <i class="done fas fa-check"></i>
+      <i class="delete fas fa-trash-alt"></i>
+    </div>
+  </div>
+  `;
+
+  todolist.insertAdjacentHTML("beforeend", divList);
+
+  const btnDone = document.querySelector(`[data-id='${doc.id}'] .done`);
+
+  btnDone.addEventListener("click", () => {
+    db.collection(`users/${userUid}/todos`).doc(`${doc.id}`).update({
+      complete: true,
+    });
+  });
+
+  const btnDelete = document.querySelector(`[data-id='${doc.id}'] .delete`);
+  btnDelete.addEventListener("click", () => {
+    db.collection(`users/${userUid}/todos`)
+      .doc(`${doc.id}`)
+      .delete()
+      .then(() => {
+        console.log("Document succesfully deleted!");
+      })
+      .catch((err) => {
+        console.log("Error removing document", err);
+      });
+  });
+}
+
 function updateUserData() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      console.log(user);
+      //console.log(user);
       userName.innerText = `Welcome ${firebase.auth().currentUser.displayName}`;
+      userUid = firebase.auth().currentUser.uid;
     } else {
     }
   });
 }
 
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    db.collection(`users/${userUid}/todos`)
+      .orderBy("creadtedAt", "desc")
+      .onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            renderUserData(change.doc);
+          }
+          if (change.type === "removed") {
+            let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+
+            todolist.removeChild(tr);
+          }
+          if (change.type === "modified") {
+            let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+
+            todolist.removeChild(tr);
+            console.log("entered modified");
+            renderUserData(change.doc);
+          }
+        });
+      });
+  } else {
+  }
+});
+
 if (addTodo) {
   addTodo.addEventListener("click", addToFirebase);
+
   function addToFirebase() {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        const ref = db.collection(
-          `users/${firebase.auth().currentUser.uid}/todos`
-        );
-        ref.add({
-          text: todo.value,
-          complete: false,
-          creadtedAt: firebase.firestore.FieldValue.serverTimestamp(),
-        });
-      } else {
-      }
-    });
+    if (todo.value === "") {
+      alert("please enter something");
+    } else {
+      const ref = db.collection(`users/${userUid}/todos`);
+      ref.add({
+        text: todo.value,
+        complete: false,
+        creadtedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    }
   }
 }
+
 if (signOut) {
   signOut.addEventListener("click", signOutFunction);
   function signOutFunction() {
@@ -66,18 +136,3 @@ if (signOut) {
       });
   }
 }
-
-/*
-
-  let tr = `
-  <div class="list green">
-  <p class="todoText">abchvsdzfxy cvsbihgdxhgrdhjjdg</p>
-  <div class="doneDelete">
-    <button class="done">Done</button>
-    <button class="delete">Delete</button>
-  </div>
-  </div>
-  `;
-  
-    todolist.insertAdjacentHTML("beforeend", tr);
-*/
